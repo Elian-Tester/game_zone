06034c4c45ad385edbaf2620869df262bff4313c
Elian-Tester <eliancruz01@gmail.com>
2022-06-28 20:39:32 -0500

    feature/GZ-4 vista login creada y archivo login.dart creado

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:gamezone/src/pages/gameInfo.dart';
 import 'package:gamezone/src/pages/listas_temp.dart';
+import 'package:gamezone/src/pages/login.dart';
+import 'package:gamezone/src/pages/signup.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/': (BuildContext context) => listTemp(),
+    '/': (BuildContext context) => login(),
     'infoGame': (BuildContext context) => gameInfo(),
+    'signup': (BuildContext context) => signup(),
+    'listTemp': (BuildContext context) => listTemp()
   };
 }
